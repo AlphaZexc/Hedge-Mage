9a199837ce4ed9cd20b1de59fb635e96e84cd040
AlphaZexc <58919139+AlphaZexc@users.noreply.github.com>
2026-02-16 18:25:53 -0600

    Delete previous spell UI

--- Assets/Scripts/Spells/SpellUI.cs
+++ Assets/Scripts/Spells/SpellUI.cs
@@ -5,27 +5,22 @@ public class SpellUI : MonoBehaviour
 {
     public static SpellUI Instance;
 
-    public TextMeshProUGUI currentSpellText;
     public SpellBookIconUI bookIcon;
 
     void Awake()
     {
         if (Instance == null) Instance = this;
         else Destroy(gameObject);
-
-        currentSpellText.text = string.Empty;
     }
 
     public void UpdateSpellUI(SpellBase spell = null)
     {
         if (spell != null)
         {
-            currentSpellText.text = spell.spellName;
             bookIcon.SetSpell(spell);
         }
         else
         {
-            currentSpellText.text = string.Empty;
             bookIcon.SetSpell(null);
         }
     }
